ea696b66522ab6a098be0901af99294a32c030f1
kranthi <donadikranthikumar@gmail.com>
2025-09-28 17:27:03 +0530

    main

--- common.sh
+++ common.sh
@@ -10,6 +10,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
+MONGODB_HOST="mongodb.kranthi.fun"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
